f2901240689cb630853485ba16d1412fdca82606
sudheer <sudheerannameti0406@gmail.com>
2023-03-25 16:28:10 +0530

    Project Added to Github

---
+++ BugTrackingSystem/Controllers/BugController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BugTrackingSystem.Models;
+
+
+namespace BugTrackingSystem.Controllers
+{
+    public class BugController : Controller
+    {
+        // GET: Bug
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Bug b)
+        {
+            BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+            db.Bugs.Add(b);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Edit(long id)
+        {
+            BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+            Bug existingbug = db.Bugs.Where(temp => temp.BugID == id).FirstOrDefault();
+            return View(existingbug);
+        }
+        [HttpPost]
+        public ActionResult Edit(Bug b)
+        {
+            BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+            Bug existingbug = db.Bugs.Where(temp => temp.BugID ==b.BugID ).FirstOrDefault();
+            existingbug.Tracker = b.Tracker;
+            existingbug.Subject = b.Subject;
+            existingbug.Description = b.Description;
+            existingbug.Status = b.Status;
+            existingbug.StartDate = b.StartDate;
+            existingbug.Priority = b.Priority;
+            existingbug.DueDate = b.DueDate;
+            existingbug.Assignee = b.Assignee;
+            existingbug.EstimatedTime = b.EstimatedTime;
+            existingbug.PercentageDone= b.PercentageDone;
+            return View();
+        }
+    }
+}
---
+++ BugTrackingSystem/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BugTrackingSystem.Models;
+
+
+namespace BugTrackingSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Main()
+        {
+
+            return View();
+        }
+        public ActionResult Index()
+        {
+            BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+            List<Bug> bugs = db.Bugs.ToList();
+            return View(bugs);
+        }
+
+       //public  ActionResult Details(long id)
+       // {
+       //     BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+       //     Bug p = db.Bugs.Where(temp => temp.BugID == id).FirstOrDefault();
+       //     return View(p);
+       // }
+        public ActionResult Detail(long id )
+        {
+            BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+            Bug p = db.Bugs.Where(temp => temp.BugID == id).FirstOrDefault();
+            return View(p);
+        }
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Bug b)
+        {
+            BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+            db.Bugs.Add(b);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Update(long id )
+        {
+            BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+            Bug extbug = db.Bugs.Where(temp => temp.BugID == id).FirstOrDefault();
+            return View(extbug);
+        }
+        [HttpPost]
+        public ActionResult Update(Bug b)
+        {
+            BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+            Bug extbug = db.Bugs.Where(temp => temp.BugID == b.BugID).FirstOrDefault();
+            extbug.Tracker = b.Tracker;
+            extbug.Subject = b.Subject;
+            extbug.Description = b.Description;
+            extbug.Status = b.Status;
+            extbug.StartDate = b.StartDate;
+            extbug.Priority = b.Priority;
+            extbug.DueDate = b.DueDate;
+            extbug.Assignee = b.Assignee;
+            extbug.EstimatedTime = b.EstimatedTime;
+            extbug.PercentageDone = b.PercentageDone;
+            db.SaveChanges();
+            return View();
+          
+        }
+        public ActionResult Delete(long id)
+        {
+            BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+            Bug extbug = db.Bugs.Where(temp => temp.BugID == id).FirstOrDefault();
+            return View(extbug);
+        }
+        [HttpPost]
+        public ActionResult Delete(long id,Bug b)
+        {
+            BugTrackingSystemEntities1 db = new BugTrackingSystemEntities1();
+            Bug extbug = db.Bugs.Where(temp => temp.BugID == id).FirstOrDefault();
+            db.Bugs.Remove(extbug);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
